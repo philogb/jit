6350ea50329492ea177a5bb98a44036eaf814684
philogb <philogb@gmail.com>
2009-06-06 17:31:15 +0200

    Got the new layout for unbalanced trees working
    But there are still some bugs with different node shapes and
    with the levelDistance parameter

--- Source/Spacetree.js
+++ Source/Spacetree.js
@@ -275,6 +275,120 @@ this.ST= (function() {
              lca = findLCA(lca, nodesInPath[i]);
          }
      };
+     
+     
+     //Layout functions
+     var slice = Array.prototype.slice;
+     
+     function movetree(node, prop, val, orn) {
+       var p = (orn == "left" || orn == "right")? "y" : "x";
+       node[prop][p] += val;
+     };
+     
+     function moveextent(extent, val) {
+         var ans = [];
+         $each(extent, function(elem) {
+             elem = slice.call(elem);
+             elem[0] += val;
+             elem[1] += val;
+             ans.push(elem);
+         });
+         return ans;
+     };
+     
+     function merge(ps, qs) {
+       if(ps.length == 0) return qs;
+       if(qs.length == 0) return ps;
+       var p = ps.shift(), q = qs.shift();
+       return [[p[0], q[1]]].concat(merge(ps, qs));  
+     };
+     
+     function mergelist(ls, def) {
+         def = def || [];
+         if(ls.length == 0) return def; 
+         var ps = ls.pop();
+         return mergelist(ls, merge(ps, def));
+     };
+     
+     function fit(ext1, ext2, subtreeOffset, siblingOffset, i) {
+         i = i || 0;
+         if(ext1.length <= i || 
+            ext2.length <= i) return 0;
+         
+         var p = ext1[i][1], q = ext2[i][0];
+         return  Math.max(fit(ext1, ext2, subtreeOffset, siblingOffset, ++i) + subtreeOffset,
+                     p - q + siblingOffset);
+     };
+     
+     function fitlistl(es, subtreeOffset, siblingOffset) {
+       function $fitlistl(acc, es, i) {
+           i = i || 0;
+           if(es.length <= i) return [];
+           var e = es[i], ans = fit(acc, e, subtreeOffset, siblingOffset);
+           return [ans].concat($fitlistl(merge(acc, moveextent(e, ans)), es, ++i));
+       };
+       return $fitlistl([], es);
+     };
+     
+     function fitlistr(es, subtreeOffset, siblingOffset) {
+       function $fitlistr(acc, es, i) {
+           i = i || 0;
+           if(es.length <= i) return [];
+           var e = es[i], ans = -fit(e, acc, subtreeOffset, siblingOffset);
+           return [ans].concat($fitlistr(merge(moveextent(e, ans), acc), es, ++i));
+       };
+       es = slice.call(es);
+       var ans = $fitlistr([], es.reverse());
+       return ans.reverse();
+     };
+     
+     function fitlist(es, subtreeOffset, siblingOffset) {
+        var esl = fitlistl(es, subtreeOffset, siblingOffset),
+            esr = fitlistr(es, subtreeOffset, siblingOffset);
+        for(var i = 0, ans = []; i < esl.length; i++) {
+            ans[i] = (esl[i] + esr[i]) / 2;
+        }
+        return ans;
+     };
+     
+     function design(node, prop, config) {
+         var orn = config.orientation;
+         var auxp = ['x', 'y'], auxs = ['width', 'height'];
+         var ind = +(orn == "left" || orn == "right");
+         var p = auxp[ind], notp = auxp[1 - ind];
+         
+         var cnode = config.Node;
+         var s = auxs[ind], nots = auxs[1 - ind];
+         var sval = (cnode.overridable && node.data["$" + s]) || cnode[s];
+         
+         var siblingOffset = config.siblingOffset;
+         var subtreeOffset = config.subtreeOffset;
+         
+         var GUtil = Graph.Util;
+         function $design(node) {
+             var trees = [], extents = [];
+             GUtil.eachSubnode(node, function(n) {
+                 if(n.exist) {
+                     var s = $design(n);
+                     trees.push(s.tree);
+                     extents.push(s.extent);
+                 }
+             });
+             var positions = fitlist(extents, subtreeOffset, siblingOffset);
+             for(var i=0, ptrees = [], pextents = []; i < trees.length; i++) {
+                 movetree(trees[i], prop, positions[i], orn);
+                 pextents.push(moveextent(extents[i], positions[i]));
+             }
+             var resultextent = [[-sval/2, sval/2]].concat(mergelist(pextents));
+             node[prop][p] = 0;
+             node[prop][notp] = node._depth * (config.levelDistance + 50);
+             return {
+               tree: node,
+               extent: resultextent  
+             };
+         };
+         $design(node);
+     };
     
     //Now define the actual class.    
     return new Class({
@@ -431,10 +545,25 @@ this.ST= (function() {
             if(!!computeLevels || !("_depth" in node))
                 Graph.Util.computeLevels(this.graph, this.root, 0, "ignore");
             getLCA.call(this, this.clickedNode);
-            this.computePositions(node, $C(0, 0), prop);
+            this.computePositions(node, prop);
         },
         
-        computePositions: function (node, initialPos, property, contracted) {
+        computePositions: function(node, prop) {
+            design(node, prop, this.config);
+            var orn = this.config.orientation;
+            var GUtil = Graph.Util, i = ['x', 'y'][+(orn == "left" || orn == "right")];
+            //absolutize
+            (function red(node) {
+                GUtil.eachSubnode(node, function(n) {
+                    if(n.exist) {
+                        n[prop][i] += node[prop][i];
+                        red(n);
+                    }
+                });
+            })(node);
+        },
+        
+        computePositions2: function (node, initialPos, property, contracted) {
             var GUtil = Graph.Util, geom = this.geom,
             contracted = (arguments.length == 3) || contracted;
     
