8dbc6a9b5aa212c6fe7b79df896d354eacb91d52
pablo flouret <quuxbaz@gmail.com>
2010-03-21 18:02:17 +0100

    HSL/HSV color manipulation functions

--- Source/Core/Core.js
+++ Source/Core/Core.js
@@ -192,6 +192,109 @@ $.hexToRgb = function(hex) {
   }
 };
 
+// hsl/hsv manipulation functions adapted from
+// http://mjijackson.com/2008/02/rgb-to-hsl-and-rgb-to-hsv-color-model-conversion-algorithms-in-javascript
+
+$.rgbToHsl = function(rgbArray) {
+  r = rgbArray[0] / 255, g = rgbArray[1] / 255, b = rgbArray[2] / 255;
+
+  var max = Math.max(r, g, b), min = Math.min(r, g, b);
+  var h, s, l = (max + min) / 2;
+
+  if(max == min) {
+    h = s = 0; // achromatic
+  } else {
+    var d = max - min;
+    s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+    switch(max){
+      case r:h = (g - b) / d + (g < b ? 6 : 0); break;
+      case g: h = (b - r) / d + 2; break;
+      case b: h = (r - g) / d + 4; break;
+    }
+    h /= 6;
+  }
+
+  return [Math.round(h*360), Math.round(s*100), Math.round(l*100)];
+}
+
+$.hslToRgb = function(hslArray){
+  var h = hslArray[0] / 360, s = hslArray[1] / 100, l = hslArray[2] / 100;
+  var r, g, b;
+
+  if(s == 0) {
+    r = g = b = l; // achromatic
+  } else {
+    function hue2rgb(p, q, t) {
+      if(t < 0) t += 1;
+      if(t > 1) t -= 1;
+      if(t < 1/6) return p + (q - p) * 6 * t;
+      if(t < 1/2) return q;
+      if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+      return p;
+    }
+
+    var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+    var p = 2 * l - q;
+    r = hue2rgb(p, q, h + 1/3);
+    g = hue2rgb(p, q, h);
+    b = hue2rgb(p, q, h - 1/3);
+  }
+
+  return [Math.round(r*255), Math.round(g*255), Math.round(b*255)];
+}
+
+$.hslToHex = function(hslArray) {
+  return $.rgbToHex($.hslToRgb(hslArray));
+}
+
+$.rgbToHsv = function (rgbArray) {
+  r = rgbArray[0]/255, g = rgbArray[1]/255, b = rgbArray[2]/255;
+  var max = Math.max(r, g, b), min = Math.min(r, g, b);
+  var h, s, v = max;
+
+  var d = max - min;
+  s = max == 0 ? 0 : d / max;
+
+  if(max == min) {
+    h = 0; // achromatic
+  } else {
+    switch(max) {
+      case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+      case g: h = (b - r) / d + 2; break;
+      case b: h = (r - g) / d + 4; break;
+    }
+    h /= 6;
+  }
+
+  return [Math.round(h*360), Math.round(s*100), Math.round(v*100)];
+}
+
+$.hsvToRgb = function(hsvArray){
+  var h = hsvArray[0] / 360, s = hsvArray[1] / 100, v = hsvArray[2] / 100;
+  var r, g, b;
+
+  var i = Math.floor(h * 6);
+  var f = h * 6 - i;
+  var p = v * (1 - s);
+  var q = v * (1 - f * s);
+  var t = v * (1 - (1 - f) * s);
+
+  switch(i % 6) {
+    case 0: r = v, g = t, b = p; break;
+    case 1: r = q, g = v, b = p; break;
+    case 2: r = p, g = v, b = t; break;
+    case 3: r = p, g = q, b = v; break;
+    case 4: r = t, g = p, b = v; break;
+    case 5: r = v, g = p, b = q; break;
+  }
+
+  return [Math.round(r*255), Math.round(g*255), Math.round(b*255)];
+}
+
+$.hsvToHex = function(hsvArray) {
+  return $.rgbToHex($.hsvToRgb(hsvArray));
+}
+
 $.destroy = function(elem) {
   $.clean(elem);
   if (elem.parentNode)
