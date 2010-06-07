9b9aed3fbc124f280483176407612b78d69689db
philogb <philogb@gmail.com>
2010-06-08 00:44:28 +0200

    documenting Area/BarChart options

--- Source/Options/Options.AreaChart.js
+++ Source/Options/Options.AreaChart.js
@@ -3,6 +3,44 @@
  *
 */
 
+/*
+  Object: Options.AreaChart
+  
+  These are the options that you can use in the <AreaChart> class. 
+  Other options included in the AreaChart are <Options.Canvas>, <Options.Label> and <Options.Tips>.
+  
+  The code for these options is as follows
+  
+  (start code js)
+
+  Options.AreaChart = {
+    animate: true,
+    offset: 25,
+    labelOffset: 3,
+    type: 'stacked',
+    selectOnHover: true,
+    showAggregates: true,
+    showLabels: true,
+    filterOnClick: false,
+    restoreOnRightClick: false
+  };
+  
+  (end code)
+  
+  Parameters:
+  
+  animate - (boolean) Default's *true*. Whether to add animated transitions when filtering/restoring stacks.
+  offset - (number) Default's *25*. Adds margin between the visualization and the canvas.
+  labelOffset - (number) Default's *3*. Adds margin between the label and the default place where it should be drawn.
+  type - (string) Default's *'stacked'*. Stack style. Posible values are 'stacked', 'stacked:gradient' to add gradients.
+  selectOnHover - (boolean) Default's *true*. If true, it will add a mark to the hovered stack.
+  showAggregates - (boolean) Default's *true*. Display the sum of the values of the different stacks.
+  showLabels - (boolean) Default's *true*. Display the name of the slots.
+  filterOnClick - (boolean) Default's *true*. Select the clicked stack by hiding all other stacks.
+  restoreOnRightClick - (boolean) Default's *true*. Show all stacks by right clicking.
+  
+*/
+  
 Options.AreaChart = {
   $extend: true,
 
--- Source/Options/Options.BarChart.js
+++ Source/Options/Options.BarChart.js
@@ -3,6 +3,44 @@
  *
 */
 
+/**/
+  Object: Options.BarChart
+  
+  These are the options that you can use in the <BarChart> class. 
+  Other options included in the BarChart are <Options.Canvas>, <Options.Label> and <Options.Tips>.
+  
+  The code for these options is as follows
+  
+  (start code js)
+
+  Options.BarChart = {
+    animate: true,
+    offset: 25, 
+    labelOffset: 3,
+    barsOffset: 0,
+    type: 'stacked',
+    hoveredColor: '#9fd4ff',
+    orientation: 'horizontal',
+    showAggregates: true,
+    showLabels: true
+  };
+  
+  (end code)
+  
+  Parameters:
+  
+  animate - (boolean) Default's *true*. Whether to add animated transitions when filtering/restoring stacks.
+  offset - (number) Default's *25*. Adds margin between the visualization and the canvas.
+  labelOffset - (number) Default's *3*. Adds margin between the label and the default place where it should be drawn.
+  barsOffset - (number) Default's *0*. Separation between bars.
+  type - (string) Default's *'stacked'*. Stack style. Posible values are 'stacked', 'stacked:gradient' to add gradients.
+  hoveredColor - (boolean|string) Default's *'#9fd4ff'*. Sets the selected color for a hovered bar stack.
+  orientation - (string) Default's 'horizontal'. Sets the direction of the bars. Possible options are 'vertical' or 'horizontal'.
+  showAggregates - (boolean) Default's *true*. Display the sum of the values of the different stacks.
+  showLabels - (boolean) Default's *true*. Display the name of the slots.
+  
+*/
+
 Options.BarChart = {
   $extend: true,
   
